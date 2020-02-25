a50e4591c335ae9788d4a4825ccbaca429e01df0
AustinMancini <mancini787@yahoo.com>
2020-02-25 15:08:12 -0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "how are you doing today?";
+        int offset = 12;
+
+        CaesarCipher caesarCipher = new CaesarCipher();
+        String cipheredMessage = caesarCipher.cipher(message, offset);
+
+        System.out.println("Message: " + message);
+        System.out.println("Offset: " + offset);
+        System.out.println("Ciphered Message: " + cipheredMessage);
+    }
+}
---
+++ src/test/java/CaesarCipherTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class CaesarCipherTest {
+
+    private CaesarCipher caesarCipher = new CaesarCipher();
+
+    @Test
+    public void testCipheredMessageWithOffsetOf12() {
+        assertEquals("tai mdq kag pauzs fapmke", caesarCipher.cipher("how are you doing today?", 12));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertEquals("", caesarCipher.cipher("", 12));
+    }
+}
